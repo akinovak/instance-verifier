80a6873fd3108b1714186b1831e98f89390e9ec7
Andrija <akinovak@gmail.com>
2022-03-11 12:08:47 +0100

    fix tests

--- test/instance-eval.ts
+++ test/instance-eval.ts
@@ -1,25 +1,28 @@
 import { expect } from "chai"
 import { ethers, run } from "hardhat"
+import { Contract } from "ethers";
 
-describe("MSM", function () {
+describe("MSM", () => {
+  let contract: Contract;
+  before(async () => {
+    // const { address: verifierAddress } = await run("deploy:verifier", { logs: false })
+    // contract = await run("deploy:semaphore-voting", { logs: false, verifier: verifierAddress })
+    // accounts = await ethers.getSigners()
+    // coordinator = await accounts[1].getAddress()
+    contract = await run("deploy:ieval", { logs: false })
+    // const BN254LibFactory = await ethers.getContractFactory("BN254")
+    // const bn254Lib = await BN254LibFactory.deploy()
+    // await bn254Lib.deployed()
+  })
+
   it("Should properly calculate msm result", async function () {
-    const BN254LibFactory = await ethers.getContractFactory("BN254")
-    const bn254Lib = await BN254LibFactory.deploy()
-    await bn254Lib.deployed()
+    const scalars = ["0xfc"];
 
-    const InstanceEvalFactory = await ethers.getContractFactory("InstanceEval", {
-      libraries: {
-        BN254: bn254Lib.address,
-      }
-    })
+    const {x, y} = await contract.evalAtOmegaLagrange(scalars);
+    // BigNumber { value: "16252154346109855224230955701219293761017024142479962933205183441609751873582" } BigNumber { value: "71209327064833399729668004637328255039959823027976608048259093432245387662" }
+    await expect(x).to.be.eq("16252154346109855224230955701219293761017024142479962933205183441609751873582")
+    await expect(y).to.be.eq("71209327064833399729668004637328255039959823027976608048259093432245387662")
 
-    const instanceEval = await InstanceEvalFactory.deploy()
-
-    await instanceEval.deployed()
-
-    const scalars = ["0xfc", "0xfc"];
-
-    const {x, y} = await instanceEval.evalAtOmegaLagrange(scalars);
     console.log(x, y);
   });
 });
